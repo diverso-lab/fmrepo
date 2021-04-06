ae7adad0bd92614c7008bfaedb566c52c2296074
David Romero <davromorg@alum.us.es>
2021-04-07 01:13:36 +0200

    feat: User interface

---
+++ app/Http/Controllers/DepositionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DepositionController extends Controller
+{
+    public function token()
+    {
+        $token = "zd3JCJs58v8zJeHX4kl8ypNvqHMT1MdrgT6de3e6jDL79GIHGhIf3tXrlggo";
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="h3 mb-2 text-gray-800">Main</h1>
+
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

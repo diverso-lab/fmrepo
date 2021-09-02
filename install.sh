910787e4b24ca83efa57111bb63b4f69df332b0d
David Romero <davromorg@alum.us.es>
2021-09-02 12:07:57 +0200

    fix: homestead

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd homestead
 vagrant box remove laravel/homestead --all
 vagrant box add laravel/homestead -c
 vagrant up
-vagrant ssh -c 'cd laravel; composer install; php artisan migrate; php artisan db:seed; php artisan key:generate'
+vagrant ssh -c 'cd fmrepo; composer install; php artisan migrate; php artisan db:seed; php artisan key:generate'
